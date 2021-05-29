175e3f44008924b4677c436b2362ec147872000f
Juan Manuel Ferreira <juanmferreira93@gmail.com>
2021-05-29 17:43:14 -0300

    Add upload and download functionality

--- dicomframework/awsservice/awsservice.py
+++ dicomframework/awsservice/awsservice.py
@@ -1,4 +1,42 @@
-# This is the main file of this folder,
-# it have the responsability to connect,
-# upload an ddownlaod files from aws,
-# download.py, upload.py and connector.py are helpers
+import boto3
+import os
+from decouple import config
+
+BUCKET_NAME = 'metadata-test-csv'
+
+
+def connect():
+    region_name = config('AWS_DEFAULT_REGION')
+    access_key = config('AWS_ACCESS_KEY_ID')
+    master_key = config('AWS_SECRET_ACCESS_KEY')
+
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name=region_name,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=master_key
+    )
+
+    print('Conneced to s3')
+    return s3
+
+
+def upload():
+    s3 = connect()
+    bucket = s3.Bucket(BUCKET_NAME)
+
+    for file in os.listdir('data/csv_files'):
+        print(f'Uploading csv_file: {file}')
+        bucket.upload_file(
+            Filename=f'data/csv_files/{file}', Key=f'csv_files/{file}')
+
+
+def download():
+    s3 = connect()
+    bucket = s3.Bucket(BUCKET_NAME)
+
+    for obj in bucket.objects.filter(Prefix='dicom_files/'):
+        file_name = obj.key.split('/')[-1]
+
+        print(f'Downloading dicom_file: {file_name}')
+        bucket.download_file(obj.key, f'data/dicom_files/{file_name}')
